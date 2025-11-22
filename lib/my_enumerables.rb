7d2f610d3ff3c88315a90bb8d5b13f7a5c638dc5
Zarifa <mamadalievazara@gmail.com>
2025-11-22 22:56:25 +0100

    Add: my_each_with_index custom enumerable

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,15 @@
 module Enumerable
-  # Your code goes here
+  def my_each_with_index
+  index = 0
+    self.my_each do |elem|
+      yield(elem, index)
+      index += 1
+    end
+  end
+  self
+
+  def my_select
+    
 end
 
 # You will first have to define my_each
@@ -7,5 +17,10 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    for item in self
+      yield(item)
+    end
+  end
+  self
 end
